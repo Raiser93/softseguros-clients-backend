dc0e9eb244890cea0c6d6741adf7e97d9f904b70
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-07 08:46:38 -0500

    Se añade comentarios

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 
+// Instancia para generar conexión a la base de datos
 const dbConfig = new Sequelize(
     'softseguros-clients', // Nombre de base de datos
     'root', // Usuario asignado a la base de datos
-    '', // Contraseña asignada a la base de datos
+    'admin', // Contraseña asignada a la base de datos
     {
-        host: 'localhost',
-        port: 3306,
-        dialect: 'mysql'
+        host: 'localhost', // Host o direccion de alojamineto de la base de datos
+        port: 3306, // Puerto
+        dialect: 'mysql' // Motor de base de datos a usar
     }
 );
 
+// Se exporta la instancia
 module.exports = dbConfig;
