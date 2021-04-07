c93aa849d4c5a4eead84e7197a046ce12376e59e
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-07 08:46:20 -0500

    Se añade body parser para leer informacion de formularios, se importa las rutas, se crea la conexion con la base de datos y  se añade comentarios

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+// Librerias
 const express = require('express');
 const global = require('./global/environment');
+const bodyParser = require('body-parser');
+
+// Base de datos
 const dbConfig = require('./config/db');
 
+// Importar rutas cliente
+const routerClient = require('./router/Client.router');
+
+// Importar modelo
+require('./models/Client.model');
+
+// Crear conexion a la base de datos
+dbConfig.sync().then(() => {
+    console.log('DB on');
+})
+.catch(err => {
+    console.log(err);
+});
+
+// Crear app express
 const app = express();
 
-require('./models/User.model');
+// Habilitar body-parser para leer info de los formularios
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Asignar ruta cliente
+app.use('/client', routerClient());
+
+// Iniciar servidor
 app.listen(global.PORT, () => {
     console.log('Server ON', global.PORT);
-    dbConfig.sync().then(() => {
-        console.log('DB on');
-    })
-    .catch(err => {
-        console.log(err);
-    })
 });
