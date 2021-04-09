88e19d6acef9d5b4322842913071e06774a6eda9
Brayan <bgiraldo020@gmail.com>
2021-04-08 23:31:01 -0500

    Se retorna el numero de documento la obtener los clientes, se retorna los datos necesarios a mostar al crear un cliente, se genera ruta de busqueda de cliente, se genera funcion para hacer la peticion a la base de datos filtrando por nombre y fecha de nacimiento

--- controllers/Client.controller.js
+++ controllers/Client.controller.js
@@ -1,10 +1,11 @@
 const ClientModel = require('../models/Client.model');
+const { Op } = require('sequelize');
 
 // Obtener todos los usuarios con estado en True
 exports.getAllClientsEnable = async (req, res) => {
     try {
         const clients = await ClientModel.findAll({
-            attributes: ['id', 'name', 'email', 'date_birth', 'createdAt'],
+            attributes: ['id', 'name', 'email', 'document_number', 'date_birth', 'createdAt'],
             where: {
                 status: true
             }
@@ -34,7 +35,13 @@ exports.createClient = async (req, res) => {
         const client = await ClientModel.create( { name, document_number, email, date_birth } );
         res.status(201).json({
             ok: true,
-            client
+            client: {   
+                name: client.name,
+                document_number: client.document_number,
+                email: client.email,
+                date_birth: client.date_birth,
+                createdAt: client.createdAt
+            }
         });
     } catch (error) {
         res.status(500).json({
@@ -94,6 +101,38 @@ exports.disableClient = async (req, res) => {
         res.status(500).json({
             ok: false,
             msg: 'Ocurrio un error al tratar de cambiar de estado un cliente',
+            error: error
+        });
+    }
+}
+
+exports.searchClient = async (req, res) => {
+    const {char} = req.query;
+    console.log(req.query);
+    try {
+        const clients = await ClientModel.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        name: {
+                            [Op.like]: `%${char}%`
+                        }
+                    },
+                    {
+                        date_birth: new Date(char)
+                    },
+                ]
+            }
+        });
+
+        res.status(200).json({
+            ok: true,
+            clients
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Ocurrio un error al tratar de obtener los clientes',
             error: error
         });
     }
--- router/Client.router.js
+++ router/Client.router.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validatorFields } = require('../middlewares/validator-fields');
 const router = Router();
 
 // Controlador
@@ -10,13 +12,24 @@ module.exports = () => {
     router.get('/all', clientController.getAllClientsEnable);
 
     // Ruta para generar un nuevo cliente
-    router.post('/create-client', clientController.createClient);
+    router.post(
+        '/create-client',
+        [//Middlawares
+            check('email', 'El correo es obligatorio').not().isEmpty(),
+            check('email', 'El correo ingresado no es valido').isEmail(),
+            validatorFields
+        ],  
+        clientController.createClient
+    );
 
     // Ruta para modificar un cliente
     router.put('/update-client/:id', clientController.updateClient);
 
     // Ruta para cambiar de estado un cliente
     router.put('/change-status-client/:id', clientController.disableClient);
+
+    // Ruta para cambiar de estado un cliente
+    router.get('/search', clientController.searchClient);
 
     return router;
 };
