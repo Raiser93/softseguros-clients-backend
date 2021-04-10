dca90c55b4afbb97a136c82d768dc5f636dd7ed5
Brayan Giraldo Loaiza <brayan_933@hotmail.com>
2021-04-09 21:50:24 -0500

    Se regresa a los valores por defecto

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 // Instancia para generar conexión a la base de datos
 const dbConfig = new Sequelize(
-    'heroku_bdac18e5d7a1e2d', // Nombre de base de datos
-    'b3d9fddd233e9e', // Usuario asignado a la base de datos
-    '4f858335', // Contraseña asignada a la base de datos
+    'softseguros-clients', // Nombre de base de datos
+    'root', // Usuario asignado a la base de datos
+    '', // Contraseña asignada a la base de datos
     {
-        host: 'us-cdbr-east-03.cleardb.com', // Host o direccion de alojamineto de la base de datos
+        host: 'localhost', // Host o direccion de alojamineto de la base de datos
         port: 3306, // Puerto
         dialect: 'mysql', // Motor de base de datos a usar
         logging: false
